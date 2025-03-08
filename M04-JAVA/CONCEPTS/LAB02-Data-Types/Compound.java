f8f562f0bd282a86469d396d185805d9d4152dae
RDK <photocromatic@gmail.com>
2025-03-08 17:49:57 -0600

    EDC Java

--- M04-JAVA/CONCEPTS/LAB02-Data-Types/Compound.java
+++ M04-JAVA/CONCEPTS/LAB02-Data-Types/Compound.java
@@ -1,16 +1,16 @@
 public class Compound {
 
-	public static void main( String args[] ) {
-		// Compound
-		// Complex
-		// String Instance (is an object)
-		String name;
-		name = "Joshua";
-		String streetName = "Reforma";
+    public static void main( String args[] ) {
+        // Compound
+        // Complex
+        // String Instance (is an object)
+        String name;
+        name = "Joshua";
+        String streetName = "Reforma";
 
-		System.out.println( name.toUpperCase() );
-		System.out.println( name.toLowerCase() );
-		System.out.println( name.toUpperCase() );
-		// System.out.println( streetName );
-	}
+        System.out.println( name.toUpperCase() );
+        System.out.println( name.toLowerCase() );
+        System.out.println( name.toUpperCase() );
+        // System.out.println( streetName );
+    }
 }
--- M04-JAVA/CONCEPTS/LAB02-Data-Types/Primitive.java
+++ M04-JAVA/CONCEPTS/LAB02-Data-Types/Primitive.java
@@ -1,24 +1,24 @@
 public class Primitive {
 
-	public static void main( String args[] ) {
-		// Declare variables
-		int age;
-		double price;
-		char gender;
-		boolean isSingle;
+    public static void main( String args[] ) {
+        // Declare variables
+        int age;
+        double price;
+        char gender;
+        boolean isSingle;
 
-		// Initialize variables
-		age = 25;
-		price = 10.50;
-		gender = 'm';
-		isSingle = true;
+        // Initialize variables
+        age = 25;
+        price = 10.50;
+        gender = 'm';
+        isSingle = true;
 
-		// Declaring and initializing variables
-		int step = 2;
+        // Declaring and initializing variables
+        int step = 2;
 
-		System.out.println( "age: " + age );
-		System.out.println( "price: " + price );
-		System.out.println( "gender: " + gender );
-		System.out.println( "isSingle: " + isSingle );
-	}
+        System.out.println( "age: " + age );
+        System.out.println( "price: " + price );
+        System.out.println( "gender: " + gender );
+        System.out.println( "isSingle: " + isSingle );
+    }
 }
